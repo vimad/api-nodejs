24da298045fb3f27bd6421b6556446228fa8f1fe
vinod.madubashana <vinod.madubashana@cloudsolution-sa.com>
2020-05-24 16:28:20 +0530

    add endpoints to get single course and create course

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,8 +1,9 @@
 const ErrorResponse = require('../utils/ErrorResponse');
 const asyncHandler = require('../middleware/async');
 const Course = require('../models/Course');
+const BootCamp = require('../models/Bootcamp');
 
-// @dec      Get cources
+// @dec      Get courses
 // @route    GET /api/v1/courses
 // @route    GET /api/v1/:bootcampId/courses
 // @access   Public
@@ -21,3 +22,39 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
 
     res.status(200).json({success: true, count: courses.length, data: courses});
 });
+
+// @dec      Get course
+// @route    GET /api/v1/courses/:id
+// @access   Public
+exports.getCourse = asyncHandler(async (req, res, next) => {
+    let query = Course.findById(req.params.id).populate({
+        path: 'bootcamp',
+        select: "name description"
+    });
+
+    const course = await query;
+    if (!course) {
+        next(new ErrorResponse(`can not find course with id ${req.params.id}`), 404)
+    }
+
+    res.status(200).json({success: true, data: course});
+});
+
+// @dec      Get courses
+// @route    POST /api/v1/:bootcampId/courses
+// @access   Private
+exports.addCourse = asyncHandler(async (req, res, next) => {
+    req.body.bootcamp = req.params.bootcampId;
+
+    const bootCamp = BootCamp.findById(req.params.bootcampId);
+
+    if (!bootCamp) {
+        next(new ErrorResponse(`can not find bootcamp with id ${req.params.id}`), 404)
+    }
+
+    const course = await Course.create(req.body);
+
+    res.status(200).json({success: true, data: course});
+});
+
+
